6fcd444a5ec868d6135e61b69e75c637ba917768
Production044 <dmitry.klimenko.work@gmail.com>
2022-08-08 19:28:53 +0100

    1rst commit

---
+++ home/14_lesson/14_HW.py
@@ -0,0 +1,25 @@
+
+def logger(func):
+
+    def wrapper(*args):
+        result = func(*args)
+        print(result)
+
+    return wrapper
+
+
+@logger
+def add(x, y):
+    return x + y
+
+
+@logger
+def square_all(*args):
+    return [arg ** 2 for arg in args]
+
+def main():
+    add(4, 5)
+    square_all(4, 5)
+
+if __name__ == '__main__':
+    main()
